a9c77789cce6d06f42b10af9b54b933dd60f5a4d
Franko <contact@frankos.org>
2013-05-12 08:23:19 -0400

    Update config.php

--- core/config.php
+++ core/config.php
@@ -4,7 +4,8 @@ $don_faucet = "FaucetDonations";
 
 // RPC Settings
 $btclogin = array("username" => "username", "password" => "pass",
-    "host" => "localhost", "port" => "9332");
+    "host" => "localhost", "port" => "7913");
+    //use port 7913 because 7912 is the peer finding port
 
 // DB Settings
 $sqlogin = array("host" => "localhost", "dbname" => "faucet", "username" =>
@@ -15,9 +16,9 @@ $minleft = 0.01; // minimum left on account
 $minsend = 0.5; // minimum allowed to send at a time
 
 // NOT IMPLEMENTED YET ...
-$minfee = 0.1; // min. hard fee on all transactions
+$minfee = 0.0001; // min. hard fee on all transactions
 $feeperc = 0.5; // fee for outgoing transactions in percentage
-$fee_account = "KgHL1urqk1roN0eX67sC"; // set to your own KEY to recieve fee´s there
+$fee_account = "KgHL1urqk1roN0eX67sC"; // set to your own KEY to recieve feeï¿½s there
 
 
 $adscaptchaID = 3170;
